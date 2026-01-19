fe072f860c8c639b40b40718103fad6fc8a4d550
LEO <71895730+Mauricio-DevOps@users.noreply.github.com>
2026-01-19 13:26:36 -0300

    Add user login role to session

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -18,6 +18,15 @@ public class LoginModel : PageModel
         {
             HttpContext.Session.SetString("auth", "ok");
             HttpContext.Session.SetString("user", Email); // ✅ novo
+            HttpContext.Session.SetString("role", "admin");
+            return RedirectToPage("/Library");
+        }
+
+        if (Email == "user@teste.com" && Password == "123")
+        {
+            HttpContext.Session.SetString("auth", "ok");
+            HttpContext.Session.SetString("user", Email);
+            HttpContext.Session.SetString("role", "user");
             return RedirectToPage("/Library");
         }
 
